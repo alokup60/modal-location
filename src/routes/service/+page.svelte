6e2af85662923b97a9e818571d05f95f67ad7adf
alokup60 <alokrajsngh@gmail.com>
2024-01-25 17:30:04 +0530

    in slider we have all cities

--- src/routes/service/+page.svelte
+++ src/routes/service/+page.svelte
@@ -1,48 +1,58 @@
 <script>
   import { cities } from "$lib/data/cities.js";
 
-  let delhi = cities.delhi;
-  let bang = cities.banglore;
-  let mumbai = cities.mumbai;
-  let noida = cities.noida;
-  let gNoida = cities.gNoida;
-  let faridabad = cities.faridabad;
-  let gurgaon = cities.gurgaon;
-  //   console.log(delhi);
-
-  let Cities = [delhi, bang, mumbai, noida, gNoida, faridabad, gurgaon];
   let allCities = [];
-
-  //store in an array and insert into an new array
-  for (let key in Cities) {
-    console.log(Cities[key]);
-    allCities.push(Cities[key]);
+  // console.log("hiii");
+  // Store in an array and insert into a new array
+  for (let key in cities) {
+    allCities.push(cities[key]);
+    // console.log(cities[key], "hii");
   }
   console.log(allCities);
+  // let delhi = cities.delhi;
+  // let bang = cities.banglore;
+  // let mumbai = cities.mumbai;
+  // let noida = cities.noida;
+  // let gNoida = cities.gNoida;
+  // let faridabad = cities.faridabad;
+  // let gurgaon = cities.gurgaon;
+  // //   console.log(delhi);
+
+  // let Cities = [delhi, bang, mumbai, noida, gNoida, faridabad, gurgaon];
+  // let allCities = [];
+
+  //store in an array and insert into an new array
+  // for (let key in Cities) {
+  //   console.log(Cities[key]);
+  //   allCities.push(Cities[key]);
+  // }
+  // console.log(allCities);
 </script>
 
-<div
-  class="flex mx-auto h-[20rem] w-11/12 overflow-x-hidden relative rounded-md mt-6"
->
+<section>
   <div
-    style="background: linear-gradient(to right, #f4f8ff 0%, #f4f8ff 50%, #fff0 50%, #fff0 100%); filter:blur(10px)"
-    class="absolute w-[6%] h-full left-0 z-50"
-  ></div>
-  {#each allCities as { id, cityName, svg }, index (id)}
+    class="flex mx-auto h-[20rem] w-11/12 overflow-x-hidden relative rounded-md mt-6"
+  >
     <div
-      class="flex flex-col justify-center w-8/12 items-center cursor-pointer mx-auto hover-container hover-effect companiesList"
-    >
-      <img src={svg} alt={cityName} class="svg-container" />
+      style="background: linear-gradient(to right, #f4f8ff 0%, #f4f8ff 50%, #fff0 50%, #fff0 100%); filter:blur(10px)"
+      class="absolute w-[6%] h-full left-0 z-50"
+    ></div>
+    {#each allCities as { id, cityName, svg }, index (id)}
+      <div
+        class="flex flex-col justify-center w-8/12 items-center cursor-pointer mx-auto hover-container hover-effect companiesList"
+      >
+        <img src={svg} alt={cityName} class="svg-container" />
 
-      <p class="font-semibold tracking-wider">{cityName}</p>
-    </div>
-    <br />
-  {/each}
-  <div
-    style="background: linear-gradient(to left, #f4f8ff 0%, #f4f8ff 50%, #fff0 50%, #fff0 100%); filter:blur(10px)"
-    class="absolute w-[6%] h-full right-0 z-10"
-  ></div>
-</div>
+        <p class="font-semibold tracking-wider">{cityName}</p>
+      </div>
+      <br />
+    {/each}
+    <div
+      style="background: linear-gradient(to left, #f4f8ff 0%, #f4f8ff 50%, #fff0 50%, #fff0 100%); filter:blur(10px)"
+      class="absolute w-[6%] h-full right-0 z-10"
+    ></div>
+  </div>
+</section>
 
 <style>
   .svg-container {
