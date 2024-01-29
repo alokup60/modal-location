77d042b3e7f4a80eb4d27c74fd69a3a6493fe8d3
alokup60 <alokrajsngh@gmail.com>
2024-01-29 19:07:48 +0530

    some comment and font-style has changed

--- src/routes/typeWriter/+page.svelte
+++ src/routes/typeWriter/+page.svelte
@@ -3,21 +3,6 @@
 </script>
 
 <section class="bg-[#ffff]">
-  <!-- {#each arr as arr, index (index)}
-    <h1>{arr}</h1>
-  {/each} -->
-
-  <!-- <div class="wrapper relative flex">
-    <div class="static-text text-[#2B6442] text-[48px] font-bold tracking-wide">
-      One Day
-    </div>
-    <ul class="dynamic-text">
-      <li><span>Morgage</span></li>
-      <li><span>Confirmation</span></li>
-      <li><span>Sanction</span></li>
-    </ul>
-  </div> -->
-
   <div class="text-box">
     <div class="static-text">One Day</div>
     <div class="dynamic-text">
@@ -29,61 +14,6 @@
     </div>
   </div>
 </section>
-
-<!-- <style>
-  section {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    min-height: 100vh;
-  }
-  .wrapper {
-    display: flex;
-  }
-  .wrapper .static-text {
-    position: relative;
-  }
-  .wrapper .dynamic-text {
-    margin-left: 15px;
-    margin-top: 10px;
-    height: 40px;
-    line-height: 40px;
-    overflow: hidden;
-  }
-  .dynamic-text li {
-    color: #f97f7f;
-    font-weight: 500;
-    font-size: 40px;
-    letter-spacing: 1.3px;
-    position: relative;
-    top: 0;
-    animation: slide 12s steps(3) infinite;
-  }
-  @keyframes slide {
-    100% {
-      top: -360px;
-    }
-  }
-  .dynamic-text li span {
-    position: relative;
-  }
-
-  .dynamic-text li span::after {
-    content: "";
-    position: absolute;
-    left: 0;
-    height: 100%;
-    background: white;
-    width: 100%;
-    animation: typing 1.5s steps(12) infinite;
-  }
-  @keyframes typing {
-    100% {
-      left: 100%;
-      margin: 0 -35px 0 35px;
-    }
-  }
-</style> -->
 
 <style>
   .text-box {
@@ -111,6 +41,7 @@
   .dynamic-text span {
     position: relative;
     color: #f97f7f;
+    font-style: italic;
     display: block;
     text-align: left;
   }
