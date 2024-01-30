919291dd7a21d65fab9ac1ddbc1d30c6850dd304
alokup60 <alokrajsngh@gmail.com>
2024-01-30 15:48:19 +0530

    emi calculator is working now

---
+++ src/routes/emi/+page.server.js
@@ -0,0 +1,49 @@
+export const actions = {
+  inputData: async ({ request }) => {
+    const data = await request.formData();
+    const loanAmt = data.get("loanAmount");
+    const rate = data.get("rateOfInterest");
+    const tenure = data.get("tenure");
+
+    return {
+      loanAmt: loanAmt,
+      rate: rate,
+      tenure: tenure,
+      sucess: true,
+    };
+  },
+
+  calculateEmi: async ({ request }) => {
+    const data = await request.formData();
+    const loanAmt = data.get("loanAmount");
+    const rate = data.get("rateOfInterest");
+    const tenure = data.get("tenure");
+
+    const calculateEMI = (loanAmt, interestRate, tenure) => {
+      const principal = loanAmt;
+      const rateOfInterest = interestRate / (12 * 100);
+      const numberOfPayments = tenure * 12;
+
+      const emi =
+        (principal *
+          rateOfInterest *
+          Math.pow(1 + rateOfInterest, numberOfPayments)) /
+        (Math.pow(1 + rateOfInterest, numberOfPayments) - 1);
+      return emi;
+    };
+    let newEmi = calculateEMI(loanAmt, rate, tenure / 12).toFixed(2);
+
+    let totalAmt = newEmi * tenure;
+    let totalInterest = totalAmt - loanAmt;
+
+    return {
+      loanAmt: loanAmt,
+      rate: rate,
+      tenure: tenure,
+      newEmi,
+      totalAmt,
+      totalInterest,
+      success: true,
+    };
+  },
+};
---
+++ src/routes/emi/+page.svelte
@@ -0,0 +1,92 @@
+<script>
+  export let form;
+  //   export let data;
+</script>
+
+<!-- {form.loanAmt} -->
+<section class="w-full flex justify-center mx-auto items-center">
+  <form
+    method="post"
+    action="?/inputData"
+    class="w-full flex justify-center mx-auto"
+  >
+    <div class="container w-4/12 flex items-center flex-col gap-4">
+      <h2 class="font-semibold text-xl">EMI Calculator</h2>
+      <div class="loanAmount flex justify-between mx-auto w-full items-center">
+        <label for="loanAmount" class="font-semibold text-md">Loan Amount</label
+        >
+        <input
+          type="number"
+          placeholder="Enter Loan Amount"
+          name="loanAmount"
+          class="px-2 py-1 rounded-md outline-none w-8/12 border"
+        />
+      </div>
+      <div
+        class="rateOfInterest flex justify-between mx-auto w-full items-center"
+      >
+        <label for="rateOfInterest" class="font-semibold text-md"
+          >Interest Rate(%)</label
+        >
+        <input
+          type="number"
+          placeholder="Enter Interest Rate"
+          name="rateOfInterest"
+          class="px-2 py-1 rounded-md outline-none w-8/12 border"
+        />
+      </div>
+      <div class="tenure flex justify-between mx-auto w-full items-center">
+        <label for="tenure" class="font-semibold text-md"
+          >Tenure (in months)</label
+        >
+        <input
+          type="number"
+          placeholder="Enter Tenure"
+          name="tenure"
+          class="px-2 py-1 rounded-md outline-none w-8/12 border"
+        />
+      </div>
+      <button
+        formaction="?/calculateEmi"
+        class="bg-green-400 px-4 py-2 rounded-md text-white font-semibold hover:bg-green-500"
+        >Calculate</button
+      >
+    </div>
+  </form>
+</section>
+
+<section class="w-full flex justify-center mx-auto items-center mt-[2rem]">
+  <div class="w-4/12 flex flex-col gap-2">
+    {#if form?.success}
+      <div class="flex w-full justify-between">
+        <span class="font-semibold text-md">Monthly EMI:</span>
+        <p><i class="fa-solid fa-indian-rupee-sign"></i> {form?.newEmi}</p>
+      </div>
+
+      <div class="flex w-full justify-between">
+        <span class="font-semibold text-md">Principal Amount:</span>
+
+        <p><i class="fa-solid fa-indian-rupee-sign"></i> {form?.loanAmt}</p>
+      </div>
+
+      <div class="flex w-full justify-between">
+        <span class="font-semibold text-md">Total Interest:</span>
+        <p>
+          <i class="fa-solid fa-indian-rupee-sign"></i>
+          {form?.totalInterest}
+        </p>
+      </div>
+      <div class="flex w-full justify-between">
+        <span class="font-semibold text-md">Total Amount:</span>
+        <p><i class="fa-solid fa-indian-rupee-sign"></i> {form?.totalAmt}</p>
+      </div>
+    {/if}
+  </div>
+</section>
+
+<style>
+  input[type="number"] {
+    /* appearance: none; */
+    display: hidden;
+  }
+</style>
